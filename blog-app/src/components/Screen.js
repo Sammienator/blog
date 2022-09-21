8d866e780113270ea79a0714f23d19b1a0870b8e
Sammienator <sammugo18@gmail.com>
2022-09-21 22:23:53 +0300

    carousel added

--- blog-app/src/components/Screen.js
+++ blog-app/src/components/Screen.js
@@ -1,22 +1,57 @@
 import React from 'react'
-import Image1 from '../assets/Image1.jpg';
-
+import Container from 'react-bootstrap/esm/Container';
+import Carousel from 'react-bootstrap/Carousel';
+import pc from '../assets/pc.jpg';
+import pc2 from '../assets/pc2.jpg';
 
 
 const Screen = () => {
   return (
     <div>
-        
+        <Container> 
+            <Carousel> 
+
+        <Carousel.Item>
+        <img width="100%" height= "100%"
+          className="d-block"
+          src={pc}
+          alt="First slide"
+        />
+        <Carousel.Caption >
+          <h1>Telling Stories that Matter</h1>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
 
 
-  
-    <div style={{backgroundImage:`url(${Image1})`}} >
-      Hello World
+
+      <Carousel.Item>
+        <img width="100%" height= "100%"
+          className="d-block"
+          src={pc2}
+          alt="second slide"
+        />
+        <Carousel.Caption >
+          <h1>Telling stories by Us!</h1>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+
+      </Carousel>
+      
+        </Container>
+
+
+   
+   
+      
+      
     </div>
 
 
 
-    </div>
+    
   )
 }
 
